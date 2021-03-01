eaf04ae58fc0bc33f40feb7037375444c39b551d
sumyu199 <69990961+sumyu199@users.noreply.github.com>
2021-03-01 09:42:56 +0000

    bb1

--- app.py
+++ app.py
@@ -65,22 +65,24 @@ def user_input_features():
     start_date = st.sidebar.date_input('start date', datetime.date(2018,1,1))
     end_date = st.sidebar.date_input("End Date", today)
     #information = st.sidebar.selectbox('Information',('Close','Volume','Canlestick'))
-    options = ['Select Indicator','Bollinger bands with RSI','MACD', 'OBV']
+    options = ['Select Indicator','Bollinger bands with RSI','MACD', 'OBV','any']
     indicator_selection1 = st.sidebar.selectbox(
-        label='Indicator1',
+        label='Indicators Combo 1',
         options=options)
 
     indicator_selection2 = st.sidebar.selectbox(
-         label = 'Indicator2',
+         label = 'Indicator Combo 2',
          options = options)
 
     indicator_selection3 = st.sidebar.selectbox(
-         label = 'Indicator3',
+         label = 'Indicator Combo 3',
          options = options)
 
     indicator_selection4 = st.sidebar.selectbox(
-          label='Indicator4',
-          options= options)
+         label = 'Indicator Combo 4',
+         options = options)
+
+
     prediction = st.sidebar.slider('Forecasting Range',1,30,5,1)
 
     return ticker, start_date, end_date,indicator_selection1,indicator_selection2,indicator_selection3,indicator_selection4,prediction
@@ -203,30 +205,56 @@ for i in indicators:
 
  # RSI (Relative Strength Index)
  # RSI
- def RSI(data, period=14, column='Close'):
-     delta = data[column].diff(1)
-     delta = delta[1:]
-     delta = delta.dropna()
-     up = delta.copy()
-     down = delta.copy()
-     up[up < 0] = 0
-     down[down > 0] = 0
-     data['up'] = up
-     data['down'] = down
-     # Calculate the average gain and the average loss(SMA)
-     AVG_Loss = SMA(data, period, column='up')
-     AVG_Gain = abs(SMA(data, period, column='down'))
-     data['AVG_Gain'] = SMA(data, period, column='up')
-     data['AVG_Loss'] = abs(SMA(data, period, column='down'))
-     RS_SMA = AVG_Gain / AVG_Loss
-     # Calculate the Relative Strength Index(RSI)
-     RSI_SMA = 100.0 - (100.0 / (1.0 + RS_SMA))
-     data['RSI_SMA'] = RSI_SMA
-     return data
- RSI(data, period=14, column='Close')
 
  if i =='Bollinger bands with RSI':
+     def RSI(data, period=14, column='Close'):
+         delta = data[column].diff(1)
+         delta = delta[1:]
+         delta = delta.dropna()
+         up = delta.copy()
+         down = delta.copy()
+         up[up < 0] = 0
+         down[down > 0] = 0
+         data['up'] = up
+         data['down'] = down
+         # Calculate the average gain and the average loss(SMA)
+         AVG_Loss = SMA(data, period, column='up')
+         AVG_Gain = abs(SMA(data, period, column='down'))
+         data['AVG_Gain'] = SMA(data, period, column='up')
+         data['AVG_Loss'] = abs(SMA(data, period, column='down'))
+         RS_SMA = AVG_Gain / AVG_Loss
+         # Calculate the Relative Strength Index(RSI)
+         RSI_SMA = 100.0 - (100.0 / (1.0 + RS_SMA))
+         data['RSI_SMA'] = RSI_SMA
+         return data
+
+
+     RSI(data, period=14, column='Close')
+
+     def buy_sell(data,close,upper_band,lower_band):
+         Buysignal = []
+         Sellsignal = []
+         flag = -1
+         for i in range(0,len(data)):
+             if data[close][i] > data[upper_band][i] and flag !=0:
+                 Sellsignal.append(data[close][i])
+                 Buysignal.append(np.nan)
+                 flag = 0
+             elif data[close][i] < data[lower_band][i] and flag !=1:
+                 Buysignal.append(data[close][i])
+                 Sellsignal.append(np.nan)
+                 flag = 1
+             else:
+                 Buysignal.append(np.nan)
+                 Sellsignal.append(np.nan)
+         return(Buysignal,Sellsignal)
+
+     b = buy_sell(data, 'Close', 'upper band','lower band')
+
+     data['BB_Buy_Signal_Price'] = b[0]
+     data['BB_Sell_Signal_Price'] = b[1]
      fig = go.Figure()
+
 # Add traces
      fig.add_trace(go.Scatter(x= data['Date'], y=data['middle band'],
                     mode='lines',
@@ -256,6 +284,8 @@ for i in indicators:
      st.header(f"Bollinger bands\n {company_name}")
      st.plotly_chart(fig)
 
+
+
      fig = go.Figure()
      fig.add_trace(go.Scatter(x=data["Date"], y=data['RSI_SMA'],
                               mode='lines',
@@ -271,115 +301,149 @@ for i in indicators:
               annotation_position="bottom right")
      st.header(f"Relative Strength Index\n {company_name}")
      st.plotly_chart(fig)
+     fig = go.Figure()
+
+     # Add traces
+     fig.add_trace(go.Scatter(x=data["Date"], y=data['RSI_SMA'],
+                              mode='lines',
+                              name='RSI_SMA',
+                              line=dict(color='Blue')))
+     fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'],
+                              mode='lines',
+                              name='Close Price',
+                              line=dict(
+                                  color='LightSkyBlue')))
+     fig.add_trace(go.Scatter(x=data['Date'], y=data['BB_Buy_Signal_Price'],
+                              mode='markers',
+                              marker_symbol='triangle-up',
+                              name='Buy',
+                              marker=dict(
+                                  color='green',
+                                  opacity=1)))
+     fig.add_trace(go.Scatter(x=data['Date'], y=data['BB_Sell_Signal_Price'],
+                              mode='markers',
+                              marker_symbol='triangle-down',
+                              name='Sell',
+                              marker=dict(
+                                  color='red',
+                                  opacity=1)))
+     fig.update_layout(
+         autosize=False,
+         width=800,
+         height=600)
+     st.header(f"Bollinger Band Signal with RSI\n {company_name}")
+     st.plotly_chart(fig)
      st.write("If price close outside of the upper Bollinger Band,Then we are going to look place a **SELL TRADE**.")
      st.write("If looking to sell,wait for **RSI > 75** Before entering")
-     st.write("If price outside of the lower Bollinger Band,Then we are going to look place a **BUY TRADE**.")
+     st.write("If price close outside of the lower Bollinger Band,Then we are going to look place a **BUY TRADE**.")
      st.write("If looking to Buy,Wait for **RSI < 25** Before entering")
 
 # ## MACD (Moving Average Convergence Divergence)
 # MACD
 
- def MACD(data,period_long = 26,period_short = 12 ,period_signal = 9, column = 'Close'):
-  #Calculate the Short term exponential moving average
-    ShortEMA = EMA(data,period_short,column = column)
-  #Calculate the Long term exponential moving average
-    LongEMA = EMA(data,period_long,column = column)
-  #Calculate the Moving Average Convergence/Divergence(MACD)
-    data['MACD'] = ShortEMA - LongEMA
-  #Calculate the signal line
-    data['Signal_Line'] = EMA(data,period_signal,column= 'MACD')
-    return data
 
- MACD(data,period_long = 26,period_short = 12 ,period_signal = 9, column = 'Close')
-
-
-
-#buy signal and sell signal
- def buy_sell(data, MACD, Signal):
-      Buysignal = []
-      Sellsignal = []
-      flag = -1
-      for i in range(0, len(data)):
-          if data[MACD][i] > data[Signal][i]:
- #bullish signal
-              if flag != 1:
-                  Buysignal.append((data['Close'])[i])
-                  Sellsignal.append(np.nan)
-                  flag = 1
-              else:
-                  Buysignal.append(np.nan)
-                  Sellsignal.append(np.nan)
-          elif data[MACD][i] < data[Signal][i]:
- #bearish signal
-              if flag != 0:
-                  Sellsignal.append((data['Close'])[i])
-                  Buysignal.append(np.nan)
-                  flag = 0
-              else:
-                  Buysignal.append(np.nan)
-                  Sellsignal.append(np.nan)
-          else:
-              Buysignal.append(np.nan)
-              Sellsignal.append(np.nan)
-      return (Buysignal, Sellsignal)
-
-
- a = buy_sell(data, 'MACD', 'Signal_Line')
-
- data['MACD_Buy_Signal_Price'] = a[0]
- data['MACD_Sell_Signal_Price'] = a[1]
 
  if i =='MACD':
-    fig = go.Figure()
+     def MACD(data, period_long=26, period_short=12, period_signal=9, column='Close'):
+         # Calculate the Short term exponential moving average
+         ShortEMA = EMA(data, period_short, column=column)
+         # Calculate the Long term exponential moving average
+         LongEMA = EMA(data, period_long, column=column)
+         # Calculate the Moving Average Convergence/Divergence(MACD)
+         data['MACD'] = ShortEMA - LongEMA
+         # Calculate the signal line
+         data['Signal_Line'] = EMA(data, period_signal, column='MACD')
+         return data
+
+
+     MACD(data, period_long=26, period_short=12, period_signal=9, column='Close')
+
+
+     # buy signal and sell signal
+     def buy_sell(data, MACD, Signal):
+         Buysignal = []
+         Sellsignal = []
+         flag = -1
+         for i in range(0, len(data)):
+             if data[MACD][i] > data[Signal][i]:
+                 # bullish signal
+                 if flag != 1:
+                     Buysignal.append((data['Close'])[i])
+                     Sellsignal.append(np.nan)
+                     flag = 1
+                 else:
+                     Buysignal.append(np.nan)
+                     Sellsignal.append(np.nan)
+             elif data[MACD][i] < data[Signal][i]:
+                 # bearish signal
+                 if flag != 0:
+                     Sellsignal.append((data['Close'])[i])
+                     Buysignal.append(np.nan)
+                     flag = 0
+                 else:
+                     Buysignal.append(np.nan)
+                     Sellsignal.append(np.nan)
+             else:
+                 Buysignal.append(np.nan)
+                 Sellsignal.append(np.nan)
+         return (Buysignal, Sellsignal)
+
+
+     a = buy_sell(data, 'MACD', 'Signal_Line')
+
+     data['MACD_Buy_Signal_Price'] = a[0]
+     data['MACD_Sell_Signal_Price'] = a[1]
+
+     fig = go.Figure()
 
     # Add traces
-    fig.add_trace(go.Scatter(x=data['Date'], y=data['MACD'],
+     fig.add_trace(go.Scatter(x=data['Date'], y=data['MACD'],
                              mode='lines',
                              name='MACD',
                              line=dict(
                                  color='Blue')))
-    fig.add_trace(go.Scatter(x=data['Date'], y=data['Signal_Line'],
+     fig.add_trace(go.Scatter(x=data['Date'], y=data['Signal_Line'],
                              mode='lines',
                              name='Signal Line',
                              line=dict(
-                                 color='Orange')))
-    fig.update_layout(
-        autosize=False,
-        width=800,
-        height=600,
-        xaxis_title="Date")
-    st.header(f"Moving Average Convergence Divergence\n {company_name}")
-    st.plotly_chart(fig)
-    fig = go.Figure()
+                               color='Orange')))
+     fig.update_layout(
+       autosize=False,
+         width=800,
+         height=600,
+         xaxis_title="Date")
+     st.header(f"Moving Average Convergence Divergence\n {company_name}")
+     st.plotly_chart(fig)
+     fig = go.Figure()
 
 # Add traces
-    fig.add_trace(go.Scatter(x= data['Date'], y=data['Close'],
+     fig.add_trace(go.Scatter(x= data['Date'], y=data['Close'],
                     mode='lines',
                     name='Close Price',
                     line = dict(
                         color = 'LightSkyBlue')))
-    fig.add_trace(go.Scatter(x= data['Date'], y=data['MACD_Buy_Signal_Price'],
+     fig.add_trace(go.Scatter(x= data['Date'], y=data['MACD_Buy_Signal_Price'],
                     mode='markers',
                     marker_symbol = 'triangle-up',
                     name='Buy',
                     marker = dict(
                         color = 'green',
                         opacity=1)))
-    fig.add_trace(go.Scatter(x= data['Date'], y=data['MACD_Sell_Signal_Price'],
+     fig.add_trace(go.Scatter(x= data['Date'], y=data['MACD_Sell_Signal_Price'],
                     mode='markers',
                     marker_symbol = 'triangle-down',
                     name='Sell',
                     marker=dict(
                              color='red',
                              opacity=1)))
-    fig.update_layout(
+     fig.update_layout(
       autosize=False,
       width=800,
       height=600)
-    st.header(f"MACD Signal\n {company_name}")
-    st.plotly_chart(fig)
-    st.write("If MACD is **above signal line with bullish signal**,Then we are going to look place a **BUY TRADE**. ")
-    st.write("If MACD is **above signal line with bullish signal**,Then we are going to look place a **SELL TRADE**. ")
+     st.header(f"MACD Signal\n {company_name}")
+     st.plotly_chart(fig)
+     st.write("If MACD is **above signal line with bullish signal**,Then we are going to look place a **BUY TRADE**. ")
+     st.write("If MACD is **above signal line with bullish signal**,Then we are going to look place a **SELL TRADE**. ")
 
 #on-balance volume
  if i == 'OBV':
